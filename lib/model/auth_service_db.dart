2f021dfad8f6cb1f29a15917e1f1b91a05651b9d
Lee <62122572+leeyukn@users.noreply.github.com>
2020-05-04 23:39:58 -0300

    Add files via upload

---
+++ lib/model/auth_service_db.dart
@@ -0,0 +1,37 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firecrud/view/home.dart';
+import 'package:firecrud/view/login.dart';
+import 'package:flutter/material.dart';
+
+class AuthService {
+  //Handle Authenticaion
+  handleAuth() {
+    return StreamBuilder(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return HomePage();
+        }
+        else {
+          return LoginPage();
+        }
+      },
+    );
+  }
+
+  //Sign Out
+  signOut() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  //Sign In
+  signIn(email, password) {
+    FirebaseAuth.instance
+      .signInWithEmailAndPassword(email: email, password: password)
+      .then((user) {
+      print('Signed In');
+    }).catchError((e) {
+      print(e);
+    });
+  }
+}
